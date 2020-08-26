84d67de4f4b870e844170498fa49c2c6fa465eca
jsdie099 <julianopaulo.santos@hotmail.com>
2020-08-26 19:57:11 -0300

    dando continuidade com o nest, agora conseguindo usar o params de maneira correta e eficiente

--- src/first-get-route/first-get-route.controller.ts
+++ src/first-get-route/first-get-route.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
-import { Controller, Get, Req, HttpModule, Res } from '@nestjs/common';
+import { Controller, Get, Req, HttpModule, Res, Param, ParamDecoratorEnhancer } from '@nestjs/common';
 
 @Controller('get')
 export class FirstGetRouteController {
     @Get(":id")
-    index(@Req() request:Request, @Res() response: Response):HttpModule{
-        console.log(request.params.id);
+    index(@Req() request:Request, @Res() response: Response, @Param() params:ParamDecoratorEnhancer, @Param('id') id:ParamDecoratorEnhancer):HttpModule{
+        console.log(request.params.id, params['id'], id);
         return response.status(200).json({message:"hello World!"}); 
     }
 }
